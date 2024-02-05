6f410e2b754bc1968d1dc6fd4e1ad70b62c6a01e
Andrey Matveev <andrei_matveev1250@mail.ru>
2024-02-05 11:39:40 +0600

    Создаёт блок отзывов

--- source/js/modules/slider/init-slider.js
+++ source/js/modules/slider/init-slider.js
@@ -1,10 +1,12 @@
 import {initSliderHero} from './hero';
 import {initSliderPrograms} from './programs';
+import {initSliderReviews} from './reviews';
 
 
 const initSliders = () => {
   initSliderHero();
   initSliderPrograms();
+  initSliderReviews();
 };
 
 export {initSliders};
--- source/js/modules/slider/reviews.js
+++ source/js/modules/slider/reviews.js
@@ -1,13 +1,15 @@
 import Swiper from 'swiper';
 import {Scrollbar, Navigation} from 'swiper/modules';
 
-const slider = document.querySelector('[data-swiper="programs"]');
-const buttonNext = document.querySelector('[data-button="next-programs"]');
-const buttonPrev = document.querySelector('[data-button="prev-programs"]');
-const scrollbar = document.querySelector('[data-scrollbar="programs"]');
+const slider = document.querySelector('[data-swiper="reviews"]');
+const buttonNext = document.querySelector('[data-button="next-reviews"]');
+const buttonPrev = document.querySelector('[data-button="prev-reviews"]');
+const scrollbar = document.querySelector('[data-scrollbar="reviews"]');
 
 const setSlider = () => new Swiper(slider, {
   modules: [Scrollbar, Navigation],
+  keyboard: true,
+  spaceBetween: 20,
   scrollbar: {
     el: scrollbar,
     draggable: true,
@@ -19,20 +21,19 @@ const setSlider = () => new Swiper(slider, {
   },
   breakpoints: {
     1440: {
-      slidesPerView: 3,
+      slidesPerView: 2,
       spaceBetween: 32,
-      allowTouchMove: false,
     },
     768: {
-      spaceBetween: 30,
       slidesPerView: 'auto',
+      spaceBetween: 30,
       scrollbar: {
         dragSize: 324,
       },
     },
     320: {
       slidesPerView: 1,
-      spaceBetween: 15,
+      spaceBetween: 30,
       scrollbar: {
         enabled: false,
       },
@@ -40,10 +41,10 @@ const setSlider = () => new Swiper(slider, {
   },
 });
 
-const initSliderPrograms = () => {
+const initSliderReviews = () => {
   if (document.body.contains(slider)) {
     setSlider();
   }
 };
 
-export {initSliderPrograms};
+export {initSliderReviews};
